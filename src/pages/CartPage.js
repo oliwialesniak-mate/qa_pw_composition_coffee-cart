e2210b1aaac27fb76e9864302bce6245e42bab7c
Oliwia Lesniak <oliwialesniak666@gmail.com>
2025-11-24 16:29:13 +0100

    Implement component-based architecture for Coffee Cart pages

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,9 +1,24 @@
-import HeaderComponent from './components/HeaderComponent';
-import CartItemsComponent from './components/CartItemsComponent';
+import { Header } from '../components/Header.js';
+import { CartList } from '../components/CartList.js';
+import { TotalCount } from '../components/TotalCount.js';
 
-export default class CartPage {
-  constructor() {
-    this.header = new HeaderComponent();
-    this.cartItems = new CartItemsComponent();
+export class CartPage {
+  constructor(page) {
+    this.page = page;
+    this.header = new Header(page);
+    this.cartList = new CartList(page);
+    this.totalCount = new TotalCount(page);
+  }
+
+  async open() {
+    await this.page.goto('/cart');
+  }
+
+  async waitForLoading() {
+    await this.page.waitForSelector('.cart-loaded'); // adjust selector as needed
+  }
+
+  async reload() {
+    await this.page.reload();
   }
 }
--- src/pages/MenuPage.js
+++ src/pages/MenuPage.js
@@ -1,7 +1,18 @@
-import HeaderComponent from './components/HeaderComponent';
+import { Header } from '../components/Header.js';
+import { Cup } from '../components/Cup.js';
+import { TotalCount } from '../components/TotalCount.js';
+import { Promo } from '../components/Promo.js';
 
-export default class MenuPage {
-  constructor() {
-    this.header = new HeaderComponent();
+export class MenuPage {
+  constructor(page) {
+    this.page = page;
+    this.header = new Header(page);
+    this.cup = new Cup(page);
+    this.totalCount = new TotalCount(page);
+    this.promo = new Promo(page);
+  }
+
+  async open() {
+    await this.page.goto('/menu');
   }
 }
